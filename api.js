1931e7fe0dabbf68c8528b202333c65ff9443a31
barryjung <wjdgusrbs5@gmail.com>
2023-05-11 11:00:20 +0900

    feat : Create article create function
    글쓰기 버튼, 페이지 추가하였습니다.
    아티클 작성 잘 동작합니다.

--- api.js
+++ api.js
@@ -116,3 +116,33 @@ async function getArticles(){
     }
 
 }
+
+
+async function postArticle(){
+    const title = document.getElementById("title").value
+    const content = document.getElementById("content").value
+    // const image = document.getElementById("image").files[0]
+
+    const formdata = new FormData();
+
+    formdata.append('title', title)
+    formdata.append('content', content)
+    // formdata.append('image', image)
+
+    let token = localStorage.getItem("access")
+
+    const response = await fetch(`${backend_base_url}/article/`, {
+        method: 'POST',
+        headers: {
+            "Authorization": `Bearer ${token}`
+        },
+        body: formdata
+    })
+
+    if(response.status == 201) {
+        alert("글작성 완료!")
+        window.location.replace(`${frontend_base_url}/`);
+    } else {
+        alert(response.status)
+    }
+}
