98a8107b5239ed176ea05f78ec2d3282c4add392
2017250297-choi <97ckdtn@gmail.com>
2023-05-13 18:02:39 +0900

    Feat : Create Load Profile
    프로필페이지의 정보를 불러오는 코드를 작성하였습니다

--- scripts/api.js
+++ scripts/api.js
@@ -277,3 +277,25 @@ async function deleteArticle(articleId){
 //         alert(response.status)
 //     }
 // }
+
+async function getProfile(userId){
+    let token = localStorage.getItem("access")
+    const response = await fetch(`${backend_base_url}/user/${userId}/`)
+    if(response.status==200){
+        const response_json = await response.json()
+        return response_json
+    }else{
+        alert("불러오는데 실패했습니다")
+    }
+}
+async function getProfileArticle(userId){
+    let token = localStorage.getItem("access")
+    const response = await fetch(`${backend_base_url}/user/${userId}/article/`)
+    if(response.status==200){
+        console.log(response.body)
+        const response_json = await response.json()
+        return response_json
+    }else{
+        alert("불러오는데 실패했습니다")
+    }
+}
